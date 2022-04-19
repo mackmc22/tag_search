56bfcc959c4b5bb17d256b131cb8d49ed97eed5b
mackmc22 <mackmc22@gmail.com>
2022-04-19 17:24:12 -0400

    added new layer

--- tag_search.py
+++ tag_search.py
@@ -2,10 +2,10 @@ def tag_search(q, tags):
 
     matches = []
 
-    for tag in tags:
-
-        if q in tag['item']:
-            matches.append(tag)
+    for term in q:
+        for tag in tags:
+            if term in tag['item']:
+                matches.append(tag)
 
     return matches
 
@@ -26,7 +26,7 @@ assert result['item'] == 'oranges'
 
 result = tag_search('doesnt exist', [])
 assert result == None
-'''
+
 results = tag_search("app", tags)
 assert len(results) == 2
 
@@ -35,3 +35,7 @@ assert len(results) == 3
 
 results = tag_search("poop", tags)
 assert len(results) == 0
+'''
+
+results = tag_search(["app", "es", "poop"], tags)
+assert len(results) == 5
